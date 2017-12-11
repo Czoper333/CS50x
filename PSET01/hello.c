d337abfd6e5e10b320311dbf652617b6f49e46de
x.oper <leszek.czuba@gmail.com>
2017-12-11 16:09:59 +0100

    hello.c

---
+++ PSET01/hello.c
@@ -0,0 +1,10 @@
+/*This program prints out on the screen a simple greeting to the user
+  This program is written by x.oper and this code is available
+  on the git repository named "CS50x" at https://github.com/Czoper333*/
+
+#include <stdio.h.>
+
+int main(void)
+{
+    printf("Hello, world!\n");
+}
