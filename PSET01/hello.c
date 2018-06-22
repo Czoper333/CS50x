cb6e44ff6bc792a1841de2e3d92c24d8eab890e7
leszekadam <leszek.czuba@gmail.com>
2018-06-22 08:17:36 +0200

    Update hello.c

--- PSET01/hello.c
+++ PSET01/hello.c
@@ -1,6 +1,4 @@
-/*This program prints out on the screen a simple greeting to the user
-  This program is written by x.oper and this code is available
-  on the git repository named "CS50x" at https://github.com/Czoper333*/
+/*This program prints out on the screen a simple greeting to the user */
 
 #include <stdio.h.>
 
